606e0e0f1327cf5f97e434b1d751522431a0cbb7
ForhadCU <kashem.tipo@gmail.com>
2024-10-21 21:55:09 +0600

    Add : Routes and some Libraries

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import '../modules/authentication/bindings/authentication_binding.dart';
 import '../modules/authentication/views/authentication_view.dart';
+import '../modules/cart/bindings/cart_binding.dart';
+import '../modules/cart/views/cart_view.dart';
 import '../modules/dashboard/bindings/dashboard_binding.dart';
 import '../modules/dashboard/views/dashboard_view.dart';
 import '../modules/home/bindings/home_binding.dart';
@@ -10,6 +12,12 @@ import '../modules/navigation_menu/bindings/navigation_menu_binding.dart';
 import '../modules/navigation_menu/views/navigation_menu_view.dart';
 import '../modules/onboarding/bindings/onboarding_binding.dart';
 import '../modules/onboarding/views/onboarding_view.dart';
+import '../modules/order/bindings/order_binding.dart';
+import '../modules/order/views/order_view.dart';
+import '../modules/order_review/bindings/order_review_binding.dart';
+import '../modules/order_review/views/order_review_view.dart';
+import '../modules/product_details/bindings/product_details_binding.dart';
+import '../modules/product_details/views/product_details_view.dart';
 
 part 'app_routes.dart';
 
@@ -43,6 +51,26 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.PRODUCT_DETAILS,
+      page: () => const ProductDetailsView(),
+      binding: ProductDetailsBinding(),
+    ),
+    GetPage(
+      name: _Paths.CART,
+      page: () => const CartView(),
+      binding: CartBinding(),
+    ),
+    GetPage(
+      name: _Paths.ORDER,
+      page: () => const OrderView(),
+      binding: OrderBinding(),
+    ),
+    GetPage(
+      name: _Paths.ORDER_REVIEW,
+      page: () => const OrderReviewView(),
+      binding: OrderReviewBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -8,6 +8,10 @@ abstract class Routes {
   static const ONBOARDING = _Paths.ONBOARDING;
   static const NAVIGATION_MENU = _Paths.NAVIGATION_MENU;
   static const HOME = _Paths.HOME;
+  static const PRODUCT_DETAILS = _Paths.PRODUCT_DETAILS;
+  static const CART = _Paths.CART;
+  static const ORDER = _Paths.ORDER;
+  static const ORDER_REVIEW = _Paths.ORDER_REVIEW;
 }
 
 abstract class _Paths {
@@ -17,4 +21,8 @@ abstract class _Paths {
   static const ONBOARDING = '/onboarding';
   static const NAVIGATION_MENU = '/navigation-menu';
   static const HOME = '/home';
+  static const PRODUCT_DETAILS = '/product-details';
+  static const CART = '/cart';
+  static const ORDER = '/order';
+  static const ORDER_REVIEW = '/order-review';
 }
